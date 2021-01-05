9f1d0b2776077c51ed0290bd1fd9c1e4d52fd2f8
Matt <matt.hu1@outlook.com>
2021-01-05 18:08:35 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyaddons", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Matt H",
     author_email="matt.hu1@outlook.com",
     description="A collection of addons for the Python programming language",
